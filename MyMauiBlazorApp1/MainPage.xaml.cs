dc32721cf54537f9d5598c9554bc935e3fffddb7
Le Dang Vu <ledangvu@gmail.com>
2022-05-23 17:20:14 +0700

    add blazor app

---
+++ MyMauiBlazorApp1/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMauiBlazorApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyMauiBlazorApp1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace MyMauiBlazorApp1;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
