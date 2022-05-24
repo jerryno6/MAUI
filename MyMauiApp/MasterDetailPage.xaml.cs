53d5c8c40080cb31602176d53e2a27ea73f5660e
vule <ledangvu@gmail.com>
2022-05-24 13:36:11 +0700

    test flyout page

---
+++ MyMauiApp/MasterDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MyMauiApp;
+
+public partial class MasterDetailPage : FlyoutPage
+{
+	public MasterDetailPage()
+	{
+		InitializeComponent();
+
+		var listPage = new LoginPage() { Title = "LoginPageTitle" };
+		this.Flyout = listPage;
+
+		var detailPage = new LoginPage() { Title = "DetailPageTitle" };
+		this.Detail = detailPage;
+	}
+}
